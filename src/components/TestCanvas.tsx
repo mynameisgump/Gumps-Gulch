6fc45710c8a930c9544b270e26bb16fe892b6bd3
Ethan Crann <ecrann@protonmail.com>
2023-09-30 18:15:36 -0230

    He Scrollin

--- src/components/TestCanvas.tsx
+++ src/components/TestCanvas.tsx
@@ -1,23 +1,31 @@
+
 import { Canvas } from "@react-three/fiber"
 import { useGLTF } from "@react-three/drei";
 import { OrbitControls, Environment, ScrollControls } from "@react-three/drei";
+import { useEffect, useRef } from "react";
+import { degToRad } from "three/src/math/MathUtils";
+import { Mesh } from "three";
 
 
 const TestCanvas = () => {
     const { scene } = useGLTF("/3d/LiveSpinner.glb");
-    return(
-    <Canvas>
-      {/* <ambientLight intensity={5}></ambientLight> */}
-      <primitive object={scene}></primitive>
+    const meshRef = useRef<Mesh>();
 
-      <Environment preset='city'></Environment>
-      {/* <pointLight position={[10, 10, 10]}></pointLight> */}
-      {/* <mesh>
-        <boxGeometry args={[1, 1, 1]}></boxGeometry>
-        <meshStandardMaterial color="hotpink"></meshStandardMaterial>
-      </mesh> */}
-      {/* <OrbitControls></OrbitControls> */}
-    </Canvas>
+
+    useEffect(() => {
+      document.body.addEventListener("scroll", () => {
+        console.log("scrolling");
+        if (!meshRef.current) return;
+        meshRef.current.rotation.y -= 0.01;
+      })
+      console.log("mounted");
+    },[])
+
+    return(
+      <Canvas>
+        <primitive ref={meshRef} rotation={[degToRad(90),0,0]} object={scene}></primitive>
+        <Environment preset='city'></Environment>
+      </Canvas>
     )
 }
 
