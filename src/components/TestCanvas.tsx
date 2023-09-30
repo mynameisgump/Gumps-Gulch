99a47c7ea13062838374e7125a542c7b931901e5
Ethan Crann <ecrann@protonmail.com>
2023-09-30 18:41:27 -0230

    Poppin off

--- src/components/TestCanvas.tsx
+++ src/components/TestCanvas.tsx
@@ -6,23 +6,24 @@ import { useEffect, useRef } from "react";
 import { degToRad } from "three/src/math/MathUtils";
 import { Mesh } from "three";
 
-
+// Thank you for my boi:
+// https://sketchfab.com/3d-models/live-spinner-d08700291fce490ea0103943b1e2101e
 const TestCanvas = () => {
     const { scene } = useGLTF("/3d/LiveSpinner.glb");
     const meshRef = useRef<Mesh>();
 
 
     useEffect(() => {
-      document.body.addEventListener("scroll", () => {
+      document.body.addEventListener("wheel", (e) => {
         console.log("scrolling");
         if (!meshRef.current) return;
-        meshRef.current.rotation.y -= 0.01;
+        meshRef.current.rotation.y -= e.deltaY*0.005;
       })
       console.log("mounted");
     },[])
 
     return(
-      <Canvas>
+      <Canvas style={{pointerEvents: "none", position:"fixed", bottom: "-500px", left: "-500px", width: "1000px", height: "1000px", pointerEvents: "none"}}>
         <primitive ref={meshRef} rotation={[degToRad(90),0,0]} object={scene}></primitive>
         <Environment preset='city'></Environment>
       </Canvas>
