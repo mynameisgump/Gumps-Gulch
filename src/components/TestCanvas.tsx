af44a61df4c3271f7234a70816a161245eb8d794
Ethan Crann <ecrann@protonmail.com>
2023-09-30 17:46:44 -0230

    Slowly figuring it out

--- src/components/TestCanvas.tsx
+++ src/components/TestCanvas.tsx
@@ -1,16 +1,22 @@
 import { Canvas } from "@react-three/fiber"
-import { useGLTF } from "@react-three/drei"
+import { useGLTF } from "@react-three/drei";
+import { OrbitControls, Environment, ScrollControls } from "@react-three/drei";
+
+
 const TestCanvas = () => {
-    const { scene } = useGLTF("/public/assets/models/LiveSpin.glb")
+    const { scene } = useGLTF("/3d/LiveSpinner.glb");
     return(
     <Canvas>
-    <primitive object={scene} />
-      <ambientLight></ambientLight>
-      <pointLight position={[10, 10, 10]}></pointLight>
-      <mesh>
+      {/* <ambientLight intensity={5}></ambientLight> */}
+      <primitive object={scene}></primitive>
+
+      <Environment preset='city'></Environment>
+      {/* <pointLight position={[10, 10, 10]}></pointLight> */}
+      {/* <mesh>
         <boxGeometry args={[1, 1, 1]}></boxGeometry>
         <meshStandardMaterial color="hotpink"></meshStandardMaterial>
-      </mesh>
+      </mesh> */}
+      {/* <OrbitControls></OrbitControls> */}
     </Canvas>
     )
 }
