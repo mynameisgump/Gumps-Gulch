52fa0874137ca1644b64474b2cae88bfa7d0ec7b
Ethan Crann <ecrann@protonmail.com>
2023-10-01 14:51:10 -0230

    Doubles GLB's due to weird Glitch, base fidgets are working

--- src/components/TestCanvas.tsx
+++ src/components/TestCanvas.tsx
@@ -5,28 +5,43 @@ import { OrbitControls, Environment, ScrollControls } from "@react-three/drei";
 import { useEffect, useRef } from "react";
 import { degToRad } from "three/src/math/MathUtils";
 import { Mesh } from "three";
+import { LiveSpinner } from "./LiveSpinner";
 
 // Thank you for my boi:
 // https://sketchfab.com/3d-models/live-spinner-d08700291fce490ea0103943b1e2101e
 const TestCanvas = () => {
-    const { scene } = useGLTF("/3d/LiveSpinner.glb");
-    const meshRef = useRef<Mesh>();
+    const leftGltf = useGLTF("/3d/LiveSpinnerLeft.glb");
+    const rightGltf = useGLTF("/3d/LiveSpinnerRight.glb");
+    const leftMeshRef = useRef<Mesh>();
+    const rightMeshRef = useRef<Mesh>();
 
 
     useEffect(() => {
       document.body.addEventListener("wheel", (e) => {
         console.log("scrolling");
-        if (!meshRef.current) return;
-        meshRef.current.rotation.y -= e.deltaY*0.005;
+        if (!leftMeshRef.current || !rightMeshRef.current) return;
+        leftMeshRef.current.rotation.y -= e.deltaY*0.005;
+        rightMeshRef.current.rotation.y += e.deltaY*0.005;
+
       })
       console.log("mounted");
     },[])
 
     return(
+      <>
       <Canvas style={{pointerEvents: "none", position:"fixed", bottom: "-500px", left: "-500px", width: "1000px", height: "1000px", pointerEvents: "none"}}>
-        <primitive ref={meshRef} rotation={[degToRad(90),0,0]} object={scene}></primitive>
+        <primitive ref={leftMeshRef} rotation={[degToRad(90),0,0]} object={leftGltf.scene}></primitive>
         <Environment preset='city'></Environment>
       </Canvas>
+
+      <Canvas style={{pointerEvents: "none", position:"fixed", bottom: "-500px", right: "-500px", width: "1000px", height: "1000px", pointerEvents: "none"}}>
+      {/* <primitive ref={meshRef} rotation={[degToRad(90),0,0]} object={scene}></primitive> */}
+  
+          {/* <LiveSpinner ></LiveSpinner> */}
+          <primitive ref={rightMeshRef} rotation={[degToRad(90),0,0]} object={rightGltf.scene}></primitive>
+        <Environment preset='city'></Environment>
+      </Canvas>  
+      </>   
     )
 }
 
