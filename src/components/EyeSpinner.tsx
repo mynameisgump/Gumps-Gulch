ff1f3ca67b826d8989c60273fc4dcbc379e0aa5d
Ethan Crann <ecrann@protonmail.com>
2023-10-02 08:44:12 -0230

    Have a semi working versions eyes mess up when window changes however

---
+++ src/components/EyeSpinner.tsx
@@ -0,0 +1,49 @@
+import { useGLTF } from "@react-three/drei";
+import { useFrame, useThree } from "@react-three/fiber";
+import { useRef } from "react";
+import { degToRad } from "three/src/math/MathUtils";
+import { useEffect } from "react";
+import * as THREE from "three";
+
+
+var mouseX = 0, mouseY = 0;
+
+
+const EyeSpinner = () => {
+    const gltf = useGLTF("/3d/LiveSpinnerLeftEyes.glb");
+    const spinnerRef = useRef<THREE.Mesh>();
+    const viewport = useThree((state) => state.viewport)
+
+    useEffect(() => {
+        document.body.addEventListener("mousemove", (e) => {          
+          mouseX = (e.clientX / window.innerWidth) * 2 - 1;
+          mouseY = -(e.clientY / window.innerHeight) * 2 + 1;
+          // leftMeshRef.current.children[2].lookAt(target);
+          // console.log(mouseX, mouseY);
+
+        });
+    },[])
+
+    useEffect(() => {
+        if (spinnerRef.current){
+            (spinnerRef.current.children[4] as THREE.Mesh).geometry.applyMatrix4( new THREE.Matrix4().makeRotationFromEuler( new THREE.Euler( 3*Math.PI/2, Math.PI, 0 ) ) );
+
+        }
+    }, [spinnerRef.current.children[4]])
+
+    useFrame(({ mouse, viewport }) => {
+        console.log("Mousey", mouse)
+        const x = (mouseX * viewport.width) / 2.5
+        const y = (mouseY * viewport.height) / 2.5
+        if (!spinnerRef.current) return;
+        console.log(spinnerRef)
+        spinnerRef.current.children[4].lookAt(x, y, 10)
+        // spinnerRef.current.material.color.lerp(hovered ? lime : black, 0.05)
+    })
+    
+    return (
+        <primitive ref={spinnerRef} rotation={[degToRad(90),0,0]} object={gltf.scene.clone()}></primitive>
+    )
+};
+
+export default EyeSpinner
--- src/components/ScrollSpinners.tsx
+++ src/components/ScrollSpinners.tsx
@@ -7,6 +7,8 @@ import { degToRad } from "three/src/math/MathUtils";
 import { Vector3 } from "three";
 import { useThree } from "@react-three/fiber";
 import { PerspectiveCamera } from "@react-three/drei";
+import EyeSpinner from "./EyeSpinner";
+import { GizmoHelper, GizmoViewport } from "@react-three/drei";
 
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
@@ -41,7 +43,7 @@ const ScrollSpinners = () => {
         
         target.z = 1;
         // leftMeshRef.current.children[2].lookAt(target);
-        console.log(mouseX, mouseY);
+        // console.log(mouseX, mouseY);
       });
     },[])
 
@@ -51,13 +53,25 @@ const ScrollSpinners = () => {
         <group>
           <primitive ref={leftMeshRef} rotation={[degToRad(90),0,0]} object={leftScene}></primitive>
         </group>
-        
+        <GizmoHelper
+          alignment="top-right" // widget alignment within scene
+        >
+          <GizmoViewport axisColors={['red', 'green', 'blue']} labelColor="black" />
+        </GizmoHelper>
         <Environment preset='city'></Environment>
       </Canvas>
 
       <Canvas style={{pointerEvents: "none", position:"fixed", bottom: "-500px", right: "-500px", width: "1000px", height: "1000px"}}>
-          <primitive ref={rightMeshRef} rotation={[degToRad(90),0,0]} object={rightScene}></primitive>
+          <primitive ref={rightMeshRef} visible={false} rotation={[degToRad(90),0,0]} object={rightScene}></primitive>
+          <EyeSpinner></EyeSpinner>
         <Environment preset='city'></Environment>
+        <GizmoHelper
+          alignment="bottom-right" // widget alignment within scene
+          margin={[80, 80]} // widget margins (X, Y)
+        >
+          <GizmoViewport axisColors={['red', 'green', 'blue']} labelColor="black" />
+          {/* alternative: <GizmoViewcube /> */}
+        </GizmoHelper>
       </Canvas>  
       </>   
     )
