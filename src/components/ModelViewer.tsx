7e5c489bbee196f7cd8f35010586a5ed3425fe0a
TheCrypticCanadian <ecrann@protonmail.com>
2023-10-02 23:35:27 -0230

    Added slow manson fish, will cleanup for multiple models tomorrow

--- src/components/ModelViewer.tsx
+++ src/components/ModelViewer.tsx
@@ -1,18 +1,36 @@
 import { Canvas } from "@react-three/fiber"
-import { useGLTF, Environment, Clone, OrbitControls, Bounds } from "@react-three/drei"
+import { useGLTF, Environment, Clone, OrbitControls, Bounds, useAnimations } from "@react-three/drei"
+import { useEffect } from "react";
+
+const Model = () => {
+    const {scene, animations} = useGLTF("/3d/Manson-Fish-Slow.glb");
+    const { actions } = useAnimations(animations, scene);
+    // console.log(actions);
+    useEffect(() => {
+        console.log(actions)
+        if (actions){
+            actions?.Swimmin.play()
+        }
+        
+    })
+    return(
+        <>
+            <Bounds fit clip observe damping={6} margin={2}>
+                <primitive object={scene}/>
+            </Bounds>
+            <OrbitControls minPolarAngle={0} maxPolarAngle={Math.PI / 1.9} makeDefault />
+            <Environment preset='city'/>
+        </>
+
+    );
+}
 
 
 const ModelViewer = () => {
-    const gltf = useGLTF("/3d/MansonFish.glb");
-    return (
-        <Canvas style={{ width: "300px", height: "300px"}}>
-            <Bounds fit clip observe damping={6} margin={2}>
-                <Clone deep={true} object={gltf.scene}></Clone>
-            </Bounds>
-            
-            <Environment preset='city'></Environment>
-            <OrbitControls minPolarAngle={0} maxPolarAngle={Math.PI / 1.9} makeDefault />
 
+    return (
+        <Canvas style={{height: "300px"}}>
+            <Model></Model>
         </Canvas>
     )
 }
